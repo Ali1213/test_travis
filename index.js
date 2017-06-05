64795ddc53db29abbe166b9e04d6aafaa399442a
Ali1213 <yusili2000@163.com>
2017-06-05 13:46:54 +0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const express = require('express');
-const app = express();
-
-
 const fibonacci = function (n) {
   if (typeof n !== 'number') {
     throw new Error('n should be a Number');
@@ -24,18 +20,11 @@ const fibonacci = function (n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-app.get('/fib',(req,res) => {
-  var n = Number(req.query.n);
-  try{
-    res.send(String(fibonacci(n)))
-  }catch(e){
-    res.status(500).send(e.message);
-  }
-});
+exports.fibonacci = fibonacci;
 
-
-app.listen(3000,()=>{
-  console.log('app is listen on 3000')
-})
-
-module.exports = app;
+if (require.main === module) {
+  // 如果是直接执行 main.js，则进入此处
+  // 如果 main.js 被其他文件 require，则此处不会执行。
+  var n = Number(process.argv[2]);
+  console.log('fibonacci(' + n + ') is', fibonacci(n));
+}
